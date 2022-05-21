0abd126f285c312f04ec8776633d1e12d79ea290
jerrylaikr <kerui.lai@mail.mcgill.ca>
2022-05-21 17:31:24 +0000

    add animation script

---
+++ src/pyverletmd/animation_script.py
@@ -0,0 +1,142 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
+from pyverletmd.simulation_api import Many_body_system, Potential
+
+
+def main():
+    # ===Params===
+    R_1 = 7
+    R_C = 7.5
+    MASS = (
+        64 / 1000 / (6.02214076e23) * 6.242e22
+    )  # 1[kg]/atom = 6.242e22[eV*A^2*ps^-2]/atom
+    dt = 0.002
+    n_steps = 3000
+    size = [30, 30]
+
+    # initialize simulation box
+    sim = Many_body_system(size=size, potential_profile=Potential(R_1, R_C), dt=dt)
+    sim.add_atom([2, 4], [-1.98, -1.24], MASS)
+    sim.add_atom([15, 10], [-2.38, -2.02], MASS)
+    sim.add_atom([9, 6], [3.08, 2.52], MASS)
+    sim.add_atom([18, 11], [2.64, -3.0], MASS)
+    sim.add_atom([20, 20], [-2.6, 1.74], MASS)
+    sim.add_atom([24, 0], [1.24, 2.0], MASS)
+
+    # evaluate prev positions
+    sim.eval_prev_pos()
+
+    # ===Initialize Plot Canvas===
+    plt.style.use("dark_background")
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0, sim.size[0]), ylim=(0, sim.size[1]), aspect="equal")
+    time_text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
+    debug_text = ax.text(0.02, 0.75, "", transform=ax.transAxes, fontsize=8)
+
+    n_atoms = sim.n_atoms
+    traj = []
+    mark = []
+    vel_vector = []
+    force_vector = []
+    for _ in range(n_atoms):
+        traj.extend(ax.plot([], [], "c."))
+        mark.extend(ax.plot([], [], "co"))
+        vel_vector.extend(ax.plot([], [], "r-"))
+        force_vector.extend(ax.plot([], [], "y-"))
+
+    # ===Initialization Function===
+    def init():
+        for i in range(n_atoms):
+            traj[i].set_data([], [])
+            mark[i].set_data([], [])
+            vel_vector[i].set_data([], [])
+            force_vector[i].set_data([], [])
+
+        time_text.set_text("")
+        debug_text.set_text("")
+
+        return traj + mark + vel_vector + force_vector + [time_text, debug_text]
+
+    # ===Animation Function===
+    def animate(frame_idx):
+        idx = frame_idx + 1
+        print(
+            "Plotting: frame={}, idx={}, t={:.3f}ps".format(
+                frame_idx, idx, frame_idx * dt
+            )
+        )  # FOR TESTING
+        time_text.set_text(
+            "frame = {}, time = {:.3f}ps / {:.3f}ps".format(
+                frame_idx, frame_idx * dt, n_steps * dt
+            )
+        )
+
+        # update to next state
+        sim.next_step()
+        debug_text.set_text("idx = {}\n".format(idx))
+
+        # Save data for trajectory tail
+        x_traj = np.array([atom.pos_prev[0] for atom in sim.atoms_list])
+        y_traj = np.array([atom.pos_prev[1] for atom in sim.atoms_list])
+
+        # # Save data for velocity arrow
+        x_velvec = np.zeros((2, n_atoms))
+        y_velvec = np.zeros((2, n_atoms))
+        for i in range(n_atoms):
+            x_velvec[0][i] = sim.atoms_list[i].pos[0]
+            x_velvec[1][i] = sim.atoms_list[i].pos[0] + sim.atoms_list[i].vel[0]
+            y_velvec[0][i] = sim.atoms_list[i].pos[1]
+            y_velvec[1][i] = sim.atoms_list[i].pos[1] + sim.atoms_list[i].vel[1]
+
+        # # Save data for force arrow
+        x_forc = np.zeros((2, n_atoms))
+        y_forc = np.zeros((2, n_atoms))
+        for i in range(n_atoms):
+            x_forc[0][i] = sim.atoms_list[i].pos[0]
+            x_forc[1][i] = sim.atoms_list[i].pos[0] + 10 * sim.atoms_list[i].force[0]
+            y_forc[0][i] = sim.atoms_list[i].pos[1]
+            y_forc[1][i] = sim.atoms_list[i].pos[1] + 10 * sim.atoms_list[i].force[1]
+
+        # Save data for marker
+        x_mark = np.array([atom.pos[0] for atom in sim.atoms_list])
+        y_mark = np.array([atom.pos[1] for atom in sim.atoms_list])
+
+        # print("idx={}\nx_traj={}\ny_traj={}".format(idx, x_traj, y_traj)) # FOR TESTING
+        # print(pos_big_array[n_steps:max_array_size]) # FOR TESTING
+        # for i in range(6): # FOR TESTING
+        #     print(f"atom {i}:") # FOR TESTING
+        #     print(f"\tpos: {x_mark[i]}, {y_mark[i]}") # FOR TESTING
+
+        # Create 2Dlines: traj and mark, and add annotation
+        annot = []
+        for i in range(n_atoms):
+            traj[i].set_data(x_traj[i], y_traj[i])
+            mark[i].set_data(x_mark[i], y_mark[i])
+            vel_vector[i].set_data(x_velvec[:, i], y_velvec[:, i])
+            force_vector[i].set_data(x_forc[:, i], y_forc[:, i])
+            # print(str(x_mark[i]) + "," + str(y_mark[i]))
+            # Add annotation of atom id
+            annot_text = str("Atom[{}]".format(i))
+            annot.append(
+                ax.text(x_mark[i], y_mark[i] + 0.4, annot_text, color="c", fontsize=8)
+            )
+
+        return traj + mark + vel_vector + force_vector + annot + [time_text, debug_text]
+
+    # ===Call The Animator===
+    anim = animation.FuncAnimation(
+        fig,
+        animate,
+        init_func=init,
+        frames=n_steps,
+        interval=1,
+        blit=True,
+        repeat=False,
+    )
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
